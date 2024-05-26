b005f830d6110b29842fc781c1fdc6dcb188db68
Jomar Rumbawa <jomargrumbawa@gmail.com>
2024-05-26 13:44:59 -0700

    Create getComputerChoice function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!")
+const choices = ["rock", "paper", "scissors"]
+
+function getComputerChoice() {
+  const rand = Math.floor(Math.random() * choices.length)
+  return choices[rand]
+}
+
+console.log(getComputerChoice())
