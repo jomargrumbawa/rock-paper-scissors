1221a84cc39224457593e4c08e4703283a382522
Jomar Rumbawa <jomargrumbawa@gmail.com>
2024-05-26 21:37:39 -0700

    Create score variables and getHumanChoice function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 const choices = ["rock", "paper", "scissors"]
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice() {
   const rand = Math.floor(Math.random() * choices.length)
   return choices[rand]
 }
 
+function getHumanChoice() {
+  return prompt("rock, paper, scissors?").toLowerCase()
+}
+
+function playRound(humanChoice, computerScore) {
+  
+}
+
+console.log(getHumanChoice())
 console.log(getComputerChoice())
