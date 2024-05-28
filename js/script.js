8b829c10c4d082b9f4b7b98178c768cf0763858f
Jomar Rumbawa <jomargrumbawa@gmail.com>
2024-05-28 08:09:29 -0700

    Create playRound and playGame functions

--- js/script.js
+++ js/script.js
@@ -11,9 +11,33 @@ function getHumanChoice() {
   return prompt("rock, paper, scissors?").toLowerCase()
 }
 
-function playRound(humanChoice, computerScore) {
-  
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    humanScore++
+  } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+    computerScore++
+  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    humanScore++
+  } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+    computerScore++
+  } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    humanScore++
+  } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+    computerScore++
+  }
+  console.log("human choice: ", humanChoice)
+  console.log("human score: ", humanScore)
+  console.log("computer choice: ", computerChoice)
+  console.log("computer score: ", computerScore)
 }
 
-console.log(getHumanChoice())
-console.log(getComputerChoice())
+function playGame() {
+  playRound(getHumanChoice(), getComputerChoice())
+  playRound(getHumanChoice(), getComputerChoice())
+  playRound(getHumanChoice(), getComputerChoice())
+  playRound(getHumanChoice(), getComputerChoice())
+  playRound(getHumanChoice(), getComputerChoice())
+}
+
+playGame()
+
