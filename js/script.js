fca3bbcf0b545758fa5eddb53c45c2c41c57e796
Jomar Rumbawa <jomargrumbawa@gmail.com>
2024-06-03 11:10:43 -0700

    Create checkScore and resetGame functions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+const btnWrapper = document.querySelector(".btn--wrapper")
 const rockBtn = document.getElementById("rock-btn")
 const paperBtn = document.getElementById("paper-btn")
+const resetBtn = document.getElementById("reset-btn")
 const scissorsBtn = document.getElementById("scissors-btn")
 const humanScoreEl = document.getElementById("human-score")
 const computerScoreEl = document.getElementById("computer-score")
+const messageEl = document.getElementById("message")
 const choices = ["rock", "paper", "scissors"]
 let humanScore = 0
 let computerScore = 0
@@ -28,19 +31,29 @@ scissorsBtn.addEventListener("click", function() {
   playRound("scissors", getComputerChoice())
 })
 
+resetBtn.addEventListener("click", resetGame)
+
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === 'rock' && computerChoice === 'scissors') {
     humanScore++
+    messageEl.textContent = "You win! Rock beats scissors."
   } else if (humanChoice === 'rock' && computerChoice === 'paper') {
     computerScore++
+    messageEl.textContent = "You lose! Paper beats rock."
   } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
     humanScore++
+    messageEl.textContent = "You win! Scissors beats paper."
   } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
     computerScore++
+    messageEl.textContent = "You lose! Rock beats scissors."
   } else if (humanChoice === 'paper' && computerChoice === 'rock') {
     humanScore++
+    messageEl.textContent = "You win! Paper beats rock."
   } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
     computerScore++
+    messageEl.textContent = "You lose! Scissors beats paper."
+  } else {
+    messageEl.textContent = "It's a tie!"
   }
   humanScoreEl.textContent = humanScore
   computerScoreEl.textContent = computerScore
@@ -50,8 +63,23 @@ function playRound(humanChoice, computerChoice) {
 
 function checkScore() {
   if (humanScore === 5) {
-    alert('You win the game!')
+    messageEl.textContent = "You beat the computer!"
+    btnWrapper.style.display = "none"
+    resetBtn.style.display = "block"
   } else if (computerScore === 5) {
-    alert('You lose the game, computer wins!')
+    messageEl.textContent = "The computer beat you!"
+    btnWrapper.style.display = "none"
+    resetBtn.style.display = "block"
   }
+}
+
+function resetGame() {
+  console.log("here")
+  humanScore = 0
+  computerScore = 0
+  humanScoreEl.textContent = humanScore
+  computerScoreEl.textContent = computerScore
+  messageEl.textContent = "Press button to start a game"
+  resetBtn.style.display = "none"
+  btnWrapper.style.display = "block"
 }
